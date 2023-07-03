6baa03ac1983e0a378b095b1c7c0e5e944f2147f
mohamedosamam7 <memosamo7@gmail.com>
2023-07-03 22:23:25 +0300

    update age calculation logic

--- script.py
+++ script.py
@@ -1,14 +1,10 @@
 import datetime
 now = datetime.datetime.now()
-birtdate = int(input('please enter your birtdate: '))
-year = now.year
-age = year - birtdate
-print ('your age is :', age)
-
-
-
-
-        
-
-    
-    
+# input('please enter your birtdate using format yyyy/mm/dd: ')
+birtdate_str = '1990/11/03'
+birthdate_obj = datetime.datetime.strptime(birtdate_str, "%Y/%m/%d")
+age = now - birthdate_obj
+years = age.days // 365
+months = (age.days % 250) // 30
+days = (age.days % 250) % 30
+print(f'your age is : {years} year, {months} month, and {days} days')
