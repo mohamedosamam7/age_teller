2b3efad790fa9bbe728d614a02a55aa3ecc9a4f3
mohamedosamam7 <memosamo7@gmail.com>
2023-07-03 23:27:38 +0300

    add condition to prevent calling methods from other scripts

--- script.py
+++ script.py
@@ -2,21 +2,20 @@ import datetime
 
 # input('please enter your birtdate using format yyyy/mm/dd: ')
 now = datetime.datetime.now()
-birtdate_str = '1990/04/03'
+birtdate_str = '1990/04/15'
 birthdate_obj = datetime.datetime.strptime(birtdate_str, "%Y/%m/%d")
 
 
-def calculate_age_year_months_days(birth_date_obj):
+def calculate_age_year_months_days(birth_date_obj, now_obj):
     """
     input: birth_date_obj
     ouput: age by year & months & days
     """
-    now = datetime.datetime.now()
-    age = now - birthdate_obj
+    age = now_obj - birthdate_obj
     years = age.days // 365
     months = (age.days % 250) // 30
     days = (age.days % 250) % 30
-    return f'your age is : {years} year, {months} month, and {days} days'
+    return (years, months, days)
 
 
 def calculate_months_left_for_birthday(month_of_birth_date, month_now):
@@ -27,8 +26,9 @@ def calculate_months_left_for_birthday(month_of_birth_date, month_now):
     months_left = month_of_birth_date - month_now
     if months_left < 0:
         months_left = 12 + months_left
-    return f'months left for your birthday: {months_left}'
+    return months_left
 
 
-print(calculate_age_year_months_days(birthdate_obj))
-print(calculate_months_left_for_birthday(birthdate_obj.month, now.month))
+if __name__ == '__main__':
+    print(calculate_age_year_months_days(birthdate_obj, now))
+    print(calculate_months_left_for_birthday(birthdate_obj.month, now.month))
