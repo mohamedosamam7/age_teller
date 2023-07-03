62dc1b167f339b833b099793c1104ad4d2fb32c8
mohamedosamam7 <memosamo7@gmail.com>
2023-07-03 22:42:10 +0300

    add logic to calculate months left to next birthdate

--- script.py
+++ script.py
@@ -1,10 +1,16 @@
 import datetime
 now = datetime.datetime.now()
 # input('please enter your birtdate using format yyyy/mm/dd: ')
-birtdate_str = '1990/11/03'
+birtdate_str = '1990/04/03'
 birthdate_obj = datetime.datetime.strptime(birtdate_str, "%Y/%m/%d")
 age = now - birthdate_obj
 years = age.days // 365
 months = (age.days % 250) // 30
 days = (age.days % 250) % 30
 print(f'your age is : {years} year, {months} month, and {days} days')
+# 3
+# calculating months & days left till brithdate
+months_left = birthdate_obj.month - now.month
+if months_left < 0:
+    months_left = 12 + months_left
+print(f'months left for your birthday: {months_left}')
