967ada3fbdabf5fabd3b8c807ec300a52d1ea6ba
mohamedosamam7 <memosamo7@gmail.com>
2023-07-03 21:58:05 +0300

    add function to calculate age

---
+++ script.py
@@ -0,0 +1,14 @@
+import datetime
+now = datetime.datetime.now()
+birtdate = int(input('please enter your birtdate: '))
+year = now.year
+age = year - birtdate
+print ('your age is :', age)
+
+
+
+
+        
+
+    
+    
